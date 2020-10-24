23cf4f91f11f8eaf562628e463f47b0a0c0c0567
Tianyi Zheng <tianyizheng02@gmail.com>
2020-10-24 13:38:48 -0400

    Add slides to prep tab

--- frontend/patient-prep-frontend/src/app/prep-tab/prep-tab.page.ts
+++ frontend/patient-prep-frontend/src/app/prep-tab/prep-tab.page.ts
@@ -10,3 +10,25 @@ export class PrepTabPage {
   constructor() {}
 
 }
+
+@Component({
+  selector: 'slides-example',
+  template: `
+    <ion-content>
+      <ion-slides pager="true" [options]="slideOpts">
+        <ion-slide>
+          <h1>Slide</h1>
+        </ion-slide>
+      </ion-slides>
+    </ion-content>
+  `
+})
+export class SlideExample {
+  // Optional parameters to pass to the swiper instance.
+  // See http://idangero.us/swiper/api/ for valid options.
+  slideOpts = {
+    initialSlide: 1,
+    speed: 400
+  };
+  constructor() {}
+}
