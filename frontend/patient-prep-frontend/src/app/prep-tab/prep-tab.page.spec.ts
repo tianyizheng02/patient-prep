f907df92e99342d047ed2f840c15515d27b88c47
Tianyi Zheng <tianyizheng02@gmail.com>
2020-10-24 12:03:56 -0400

    Add prep tab

---
+++ frontend/patient-prep-frontend/src/app/prep-tab/prep-tab.page.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+
+import { PrepTabPage } from './prep-tab.page';
+
+describe('Tab3Page', () => {
+  let component: PrepTabPage;
+  let fixture: ComponentFixture<PrepTabPage>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [PrepTabPage],
+      imports: [IonicModule.forRoot(), ExploreContainerComponentModule]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PrepTabPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/patient-prep-frontend/src/app/prep-tab/prep-tab.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prep-tab',
+  templateUrl: 'prep-tab.page.html',
+  styleUrls: ['prep-tab.page.scss']
+})
+export class PrepTabPage {
+
+  constructor() {}
+
+}
