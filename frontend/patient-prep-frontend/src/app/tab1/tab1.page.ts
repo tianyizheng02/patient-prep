132bb3562425905fe990ddd7538b2393638463e3
Rhea Mathew <rmathew40@gatech.edu>
2020-10-24 17:47:56 -0400

    Pulling new files

--- frontend/patient-prep-frontend/src/app/tab1/tab1.page.ts
+++ frontend/patient-prep-frontend/src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Router } from '@angular/router'
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +9,52 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  nameValue: string = "";
+  sexValue: string = "";
+  ageValue: number = 0;
+  heightType: string = "ft";
+  weightType: string = "lbs";
+  heightValue1: string = ""; //ft or m
+  heightValue2: string = ""; //in or cm
+  weightValue: number = 0;
+  
+
+  constructor(private router: Router) { //heightValue: number
+    //heightValue = 0;
+  }
+
+  OnChangeHeight(heightType, heightValue1, heightValue2) {
+    if (heightType == 'ft') {
+      console.log('ft!');
+    } else {
+      console.log('meters!');
+    }
+    console.log("heightValue1", this.heightValue1,"heightValue2", this.heightValue2);
+
+  }
+
+  OnChangeWeight(weightType, weightValue) {
+    if (weightType == 'kg') {
+      console.log('kg!');
+      weightValue *= 2.205;
+    } else {
+      console.log('lbs!');
+    }
+
+    console.log("=============================")
+    console.log("weightValue", weightValue);
+  }
+
+  submitInfo() {
+    //check to see if all info is submitted
+    console.log('name',this.nameValue)
+    console.log('sex',this.sexValue)
+    console.log('age',this.ageValue);
+    console.log('heightType',this.heightType);
+    console.log('heightValue1', this.heightValue1)
+    console.log('heightValue2', this.heightValue2)
+
+
+  }
 
 }
