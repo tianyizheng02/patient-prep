7c6d62438efb26b5ea3d0b172de6501ad28beda5
Rhea Mathew <rmathew40@gatech.edu>
2020-10-24 23:45:21 -0400

    adding profile

--- frontend/patient-prep-frontend/src/app/tab1/tab1.page.ts
+++ frontend/patient-prep-frontend/src/app/tab1/tab1.page.ts
@@ -16,45 +16,58 @@ export class Tab1Page {
   weightType: string = "lbs";
   heightValue1: string = ""; //ft or m
   heightValue2: string = ""; //in or cm
+  heightValue: string = "";
   weightValue: number = 0;
   
 
-  constructor(private router: Router) { //heightValue: number
-    //heightValue = 0;
-  }
+  constructor(private router: Router) {}
 
   OnChangeHeight(heightType, heightValue1, heightValue2) {
-    if (heightType == 'ft') {
-      console.log('ft!');
-    } else {
-      console.log('meters!');
-    }
-    console.log("heightValue1", this.heightValue1,"heightValue2", this.heightValue2);
-
+    
   }
 
   OnChangeWeight(weightType, weightValue) {
     if (weightType == 'kg') {
-      console.log('kg!');
+      //update profile data after conversion
       weightValue *= 2.205;
-    } else {
-      console.log('lbs!');
     }
-
-    console.log("=============================")
-    console.log("weightValue", weightValue);
   }
 
   submitInfo() {
     //check to see if all info is submitted
-    console.log('name',this.nameValue)
-    console.log('sex',this.sexValue)
+
+    if (this.heightType == 'ft') {
+      this.heightValue = this.heightValue1 + "ft " + this.heightValue2 + "in";
+    } else {
+      this.heightValue = this.heightValue1 + "m " + this.heightValue2 + "cm";;
+    }
+
+    console.log('name',this.nameValue);
+    console.log('sex',this.sexValue);
     console.log('age',this.ageValue);
-    console.log('heightType',this.heightType);
-    console.log('heightValue1', this.heightValue1)
-    console.log('heightValue2', this.heightValue2)
-
-
+    console.log('heightValue', this.heightValue);
+    console.log('weightValue', this.weightValue, "lbs");
   }
+}
 
+@Component({
+  selector: 'slides',
+  template: `
+    <ion-content>
+      <ion-slides pager="true" [options]="slideOpts">
+        <ion-slide>
+          <h1>Slide</h1>
+        </ion-slide>
+      </ion-slides>
+    </ion-content>
+  `
+})
+export class Slide {
+  // Optional parameters to pass to the swiper instance.
+  // See http://idangero.us/swiper/api/ for valid options.
+  slideOpts = {
+    initialSlide: 1,
+    speed: 400
+  };
+  constructor() {}
 }
